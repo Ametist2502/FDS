dbb63ebe81bb602c1c461fd4f1a11c7fcd55f3e0
Ametist2502 <whaleblue2002@gmail.com>
2023-02-01 00:31:49 +0700

    up

---
+++ BalloonPopGame-main/BalloonPop.py
@@ -0,0 +1,106 @@
+# Import
+import random
+import pygame
+import cv2
+import numpy as np
+from cvzone.HandTrackingModule import HandDetector
+import time
+
+
+# Initialize
+pygame.init()
+
+# Create Window/Display
+width, height = 1280, 720
+window = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Balloon Pop")
+
+# Initialize Clock for FPS
+fps = 60
+clock = pygame.time.Clock()
+
+# Webcam
+cap = cv2.VideoCapture(0)
+cap.set(3, 1280)  # width
+cap.set(4, 720)  # height
+
+# Images
+imgBalloon = pygame.image.load('./Resources/BalloonRed.png').convert_alpha()
+rectBalloon = imgBalloon.get_rect()
+rectBalloon.x, rectBalloon.y = 500, 300
+
+# Variables
+speed = 7
+score = 0
+startTime = time.time()
+totalTime = 60
+
+# Detector
+detector = HandDetector(detectionCon=0.8, maxHands=1)
+
+
+def resetBalloon():
+    rectBalloon.x = random.randint(100, img.shape[1] - 100)
+    rectBalloon.y = img.shape[0] + 50
+
+
+# Main loop
+start = True
+while start:
+    # Get Events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            start = False
+            pygame.quit()
+
+    # Apply Logic
+    timeRemain = int(totalTime -(time.time()-startTime))
+    if timeRemain <0:
+        window.fill((255,255,255))
+
+        font = pygame.font.Font('./Resources/Marcellus-Regular.ttf', 50)
+        textScore = font.render(f'Your Score: {score}', True, (50, 50, 255))
+        textTime = font.render(f'Time UP', True, (50, 50, 255))
+        window.blit(textScore, (450, 350))
+        window.blit(textTime, (530, 275))
+
+    else:
+        # OpenCV
+        success, img = cap.read()
+        img = cv2.flip(img, 1)
+        hands, img = detector.findHands(img, flipType=False)
+
+        rectBalloon.y -= speed  # Move the balloon up
+        # check if balloon has reached the top without pop
+        if rectBalloon.y < 0:
+            resetBalloon()
+            speed += 1
+
+        if hands:
+            hand = hands[0]
+            x, y = hand['lmList'][8][0:2]
+            if rectBalloon.collidepoint(x, y):
+                resetBalloon()
+                score += 10
+                speed += 1
+
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        imgRGB = np.rot90(imgRGB)
+        frame = pygame.surfarray.make_surface(imgRGB).convert()
+        frame = pygame.transform.flip(frame, True, False)
+        window.blit(frame, (0, 0))
+        window.blit(imgBalloon, rectBalloon)
+
+        font = pygame.font.Font('./Resources/Marcellus-Regular.ttf', 50)
+        textScore = font.render(f'Score: {score}', True, (50, 50, 255))
+        textTime = font.render(f'Time: {timeRemain}', True, (50, 50, 255))
+        window.blit(textScore, (35, 35))
+        window.blit(textTime, (1000, 35))
+
+    # Update Display
+    pygame.display.update()
+    # Set FPS
+    clock.tick(fps)
+    
+
+    
---
+++ BalloonPopGame-main/README.md
@@ -0,0 +1,7 @@
+# Balloon Pop Game
+
+A balloon pop game using computer vision.
+
+Top of the index finger is used to pop the balloons that are generated at random locations to move up.
+
+<i>Reference: https://www.computervision.zone/</i>
